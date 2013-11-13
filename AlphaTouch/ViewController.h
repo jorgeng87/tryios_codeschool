1f253ed8dbd8281f66b0a0ef053d3c89ec0d73be
Jorge Núñez <jorgeng87@gmail.com>
2013-11-13 23:27:26 +0100

    Initial Commit

---
+++ AlphaTouch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AlphaTouch
+//
+//  Created by Jorge on 11/13/13.
+//  Copyright (c) 2013 Jorge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AlphaTouch/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AlphaTouch
+//
+//  Created by Jorge on 11/13/13.
+//  Copyright (c) 2013 Jorge. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
