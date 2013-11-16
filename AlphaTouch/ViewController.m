4b8d9ca36f5bed01157fbc6b452b8275579638b1
Jorge Núñez <jorgeng87@gmail.com>
2013-11-16 17:51:50 +0100

    Move view logic to ViewController

--- AlphaTouch/ViewController.m
+++ AlphaTouch/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) UIView *view;
 @end
 
 @implementation ViewController
@@ -17,7 +17,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithRed:0.462 green:0.749 blue:0.937 alpha:1.0];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)loadView
+{
+    CGRect viewRect = [[UIScreen mainScreen] bounds];
+    UIView *colorView = [[UIView alloc] initWithFrame:viewRect];
+    
+    self.view = colorView;
+
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    NSLog(@"Touch started");
 }
 
 - (void)didReceiveMemoryWarning
